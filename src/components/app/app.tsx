abc1141bd3f63edbfeab20eecac6563aed62ee40
kozikova <lenaklenina1991@gmail.com>
2024-06-25 21:40:14 +0300

    Added Home page. Done steps 1-3

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,21 +1,13 @@
 import React from "react";
 import AppHeader from "./../app-header/app-header";
 import styles from "./app.module.css";
-import BurgerConstructor from "./../burger-constructor/burger-constructor";
-import BurgerIngredients from "./../burger-ingredients/burger-ingredients";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./../../pages/Home/home";
 import { getIngredients } from "../../services/burgerIngredients";
-import ingredientType from "./../../utils/types";
-import { useDispatch, useSelector } from "react-redux";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
-import { useAppSelector } from "../../hooks/useAppSelector";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   const dispatch = useAppDispatch();
-  const { loading, error, ingredients } = useAppSelector(
-    (state) => state.ingredients
-  );
 
   React.useEffect(() => {
     dispatch(getIngredients());
@@ -24,18 +16,9 @@ function App() {
   return (
     <div className={styles.page}>
       <AppHeader />
-      <main>
-        {loading && "Загрузка..."}
-        {!loading && error && <p>Ошибка: {error}</p>}
-        {!loading && !error && ingredients.length && (
-          <section className="wrapper-columns-center">
-            <DndProvider backend={HTML5Backend}>
-              <BurgerIngredients />
-              <BurgerConstructor />
-            </DndProvider>
-          </section>
-        )}
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
